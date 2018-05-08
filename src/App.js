3d58c206bbbca9df3bb12008bc346fff08b342e6
Brian Burns <bdburns6389@gmail.com>
2018-05-08 10:14:43 -0500

    added greeting

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+	<p>This is going to be my new website!</p>
       </div>
     );
   }
